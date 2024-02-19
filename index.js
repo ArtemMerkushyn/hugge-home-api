443f3a51ada9a1d53ac9041715b868f12efb58ba
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-02-19 21:45:12 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+//routes http://localhost:8080/
+
+
+
+async function start() {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://merkushyndev13:merkushyndev13@cluster0.dwp2fvz.mongodb.net/`
+        );
+        app.listen(PORT, () => {
+            console.log(`server started on: http://localhost:${PORT}`)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
